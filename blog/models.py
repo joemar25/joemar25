2ef5bdb7caf168082b13b982a7e308a114d958e6
I am a cat <joemar.cardino000@gmail.com>
2023-05-25 02:59:18 +0800

    can query from db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    contents = models.TextField(null=True, blank=True)
+    author = models.CharField(max_length=100, default="joemar25")
+    category = models.CharField(max_length=50, null=True, blank=True)
+    tags = models.CharField(max_length=200, null=True, blank=True)
+    thumbnail = models.ImageField(upload_to='blog/blog_img/thumbnails/', blank=True, null=True)
+    images = models.ImageField(upload_to='blog/blog_img/images/', blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,10 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-
-# mar - sample contents before implementing the databse for blogs (we need a good relational simple databse)
-blog_posts = [
-    {'id':'1', 'title':'My First Blog Post', 'contents':'This is my first blog post. Welcome to my blog!', 'author':'John Doe', 'date':'2021-09-01', 'category':'Introduction', 'tags':['blogging', 'introduction'], 'images':{'1.jpg', '2.jpg'}},
-    {'id':'2', 'title':'My Favorite Python Libraries', 'contents':'In this blog post, I will share my favorite Python libraries and how I use them in my projects.', 'author':'John Doe', 'date':'2021-09-05', 'category':'Python', 'tags':['python', 'libraries'], 'images':{'3.jpg', '4.jpg'}},
-    {'id':'3', 'title':'How to Stay Motivated as a Developer', 'contents':'In this blog post, I will share my tips on how to stay motivated as a developer and avoid burnout.', 'author':'John Doe', 'date':'2021-09-10', 'category':'Motivation', 'tags':['motivation', 'developer'], 'images':{'5.jpg', '6.jpg'}},
-    {'id':'4', 'title':'My Experience with Remote Work', 'contents':'In this blog post, I will share my experience with remote work and how I stay productive while working from home.', 'author':'John Doe', 'date':'2021-09-15', 'category':'Remote Work', 'tags':['remote work', 'productivity'], 'images':{'7.jpg', '8.jpg'}}
-]
+from .models import Blog
 
 # CRUD - Create, Read <= Index, Update, Delete
 def index(request):
+    blog_posts = Blog.objects.all()
     context = {'blogs': blog_posts}
     return render(request, 'blog/index.html', context)
 
@@ -19,20 +13,22 @@ def create(request):
 
 def get_blog_post(pk):
     try:
-        return blog_posts[int(pk)-1]
-    except IndexError:
+        return Blog.objects.get(pk=pk)
+    except Blog.DoesNotExist:
         return None
 
 def get_blog_context(blog):
     return {
-        'id': blog['id'],
-        'title': blog['title'],
-        'contents': blog['contents'],
-        'author': blog['author'],
-        'date': blog['date'],
-        'category': blog['category'],
-        'tags': blog['tags'],
-        'images': blog['images'], 
+        'id': blog.id,
+        'title': blog.title,
+        'contents': blog.contents,
+        'author': blog.author,
+        'category': blog.category,
+        'tags': blog.tags,
+        'thumbnail': blog.thumbnail,
+        'images': blog.images,
+        'updated': blog.updated,
+        'created': blog.created,
     }
 
 def update(request, pk):
