2bd023dda939547d8206634c27b43d7f82a4d909
I am a cat <joemar.cardino000@gmail.com>
2023-05-30 18:16:02 +0800

    initial portfolio template by Joemar

--- application/_root/settings.py
+++ application/_root/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 
 # mar - custom
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
