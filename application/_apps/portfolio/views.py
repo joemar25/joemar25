8d00047ee0742ea7266339b25ab81fc69ec5cc25
I am a cat <joemar.cardino000@gmail.com>
2023-05-31 11:49:10 +0800

    json profile added

--- application/_apps/portfolio/views.py
+++ application/_apps/portfolio/views.py
@@ -1,4 +1,22 @@
+import json, os
+from django.conf import settings
 from django.shortcuts import render
 
+def load_json():
+    with open(os.path.join(settings.BASE_DIR, '_root', 'database', 'profile.json')) as f:
+        data = json.load(f)
+    return {
+        'full_name': data['first'] + ' ' + data['middle_initial'] + ' ' + data['last'],
+        'first_name': data['first'] + "'s",
+        'introduction': data['introduction'],
+        'description': data['description'],
+        'github_link': data['github_link'],
+        'facebook_link': data['facebook_link'],
+        'school': data['school'],
+        'likes': data['likes'],
+        'dislikes': data['dislikes'],
+    }
+
 def index(request):
-    return render(request, 'portfolio/index.html')
+    context = load_json()
+    return render(request, 'portfolio/index.html', context)
