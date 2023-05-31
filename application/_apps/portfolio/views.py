06b00f1a0a24e93d69f5efb1e1c3c9c61cc1b4a2
I am a cat <joemar.cardino000@gmail.com>
2023-05-31 21:33:14 +0800

    update

--- application/_apps/portfolio/views.py
+++ application/_apps/portfolio/views.py
@@ -12,10 +12,13 @@ def load_json():
         'description': data['description'],
         'github_link': data['github_link'],
         'facebook_link': data['facebook_link'],
+        'contact_me_description': data['contact_me_description'],
         'school': data['school'],
         'likes': data['likes'],
         'dislikes': data['dislikes'],
-        'contact_me_description': data['contact_me_description'],
+        'app_experties': data['app_experties'],
+        'known_languages': data['known_languages'],
+        'projects': data['projects'],
     }
 
 def index(request):
--- application/static/js/main.js
+++ application/static/js/main.js
@@ -4,121 +4,59 @@ $(document).ready(function () {
       $(".nav").toggleClass("open");
     });
   }
-
-  function smoothScroll() {
-    $('a[href^="#"]').click(function (event) {
-      var target = $($(this).attr("href"));
-
-      if (target.length) {
-        event.preventDefault();
-        $("html, body").animate(
-          {
-            scrollTop: target.offset().top - 15,
-          },
-          300
-        );
-      }
-
-      $(".nav").toggleClass("open");
-    });
-  }
-
-  function fullSlider() {
-    $("#full-slide .prev, #full-slide .next").click(function () {
-      var $this = $(this),
-        current = $(".banner").find(".active"),
-        position = $(".banner").children().index(current),
-        totalPics = $(".banner").children().length;
-
-      if ($this.hasClass("next")) {
-        if (position < totalPics - 1) {
-          $(".active").removeClass("active").next().addClass("active");
-        } else {
-          $(".banner li").removeClass("active").first().addClass("active");
-        }
-      } else {
-        if (position === 0) {
-          $(".banner li").removeClass("active").last().addClass("active");
-        } else {
-          $(".active").removeClass("active").prev().addClass("active");
-        }
-      }
-    });
-  }
-
-  function threeSlider() {
-    $("#three-slide .prev, #three-slide .next").click(function () {
-      var $this = $(this),
-        curBack = $(".slider").find(".back"),
-        posBack = $(".slider").children().index(curBack),
-        curCurr = $(".slider").find(".current"),
-        posCurr = $(".slider").children().index(curCurr),
-        curFront = $(".slider").find(".front"),
-        posFront = $(".slider").children().index(curFront),
-        totalPics = $(".slider").children().length;
-
-      $(".slider").addClass("swap");
-
-      setTimeout(function () {
-        if ($this.hasClass("next")) {
-          if (
-            posFront < totalPics - 1 &&
-            posCurr < totalPics - 1 &&
-            posBack < totalPics - 1
-          ) {
-            $(".back").removeClass("back").next().addClass("back");
-            $(".current").removeClass("current").next().addClass("current");
-            $(".front").removeClass("front").next().addClass("front");
-          } else {
-            if (posFront === totalPics - 1) {
-              $(".back").removeClass("back").next().addClass("back");
-              $(".current").removeClass("current").next().addClass("current");
-              $(".slider li").removeClass("front").first().addClass("front");
-            } else if (posCurr === totalPics - 1) {
-              $(".back").removeClass("back").next().addClass("back");
-              $(".slider li")
-                .removeClass("current")
-                .first()
-                .addClass("current");
-              $(".front").removeClass("front").next().addClass("front");
-            } else {
-              $(".slider li").removeClass("back").first().addClass("back");
-              $(".current").removeClass("current").next().addClass("current");
-              $(".front").removeClass("front").next().addClass("front");
-            }
-          }
-        } else {
-          if (posBack !== 0 && posCurr !== 0 && posFront !== 0) {
-            $(".back").removeClass("back").prev().addClass("back");
-            $(".current").removeClass("current").prev().addClass("current");
-            $(".front").removeClass("front").prev().addClass("front");
-          } else {
-            if (posBack === 0) {
-              $(".slider li").removeClass("back").last().addClass("back");
-              $(".current").removeClass("current").prev().addClass("current");
-              $(".front").removeClass("front").prev().addClass("front");
-            } else if (posCurr === 0) {
-              $(".back").removeClass("back").prev().addClass("back");
-              $(".slider li").removeClass("current").last().addClass("current");
-              $(".front").removeClass("front").prev().addClass("front");
-            } else {
-              $(".back").removeClass("back").prev().addClass("back");
-              $(".current").removeClass("current").prev().addClass("current");
-              $(".slider li").removeClass("front").last().addClass("front");
-            }
-          }
-        }
-
-        $(".slider").removeClass("swap");
-      }, 300);
-    });
-  }
-
   nav();
-
-  smoothScroll();
-
-  fullSlider();
-
-  threeSlider();
 });
+
+// function colorChanger() {
+  
+//   // color scheme selection (atleast 10) for every 5 seconds for now (supposed 5 mins)
+//   const colorSchemeLight = [
+//     ["#cccccc", "#abc", "#cccccc", "#cccccc", "#cccccc"],
+//     ["#cccccc", "#cccccc", "#cccccc", "#cccccc", "#cccccc"],
+//     ["#cccccc", "#cccccc", "#cccccc", "#cccccc", "#cccccc"],
+//     ["#cccccc", "#cccccc", "#cccccc", "#cccccc", "#cccccc"],
+//     ["#cccccc", "#cccccc", "#cccccc", "#cccccc", "#cccccc"],
+//   ]
+
+//   const colorSchemeDark = [
+//     ["#cccccc", "#cccccc", "#cccccc", "#cccccc", "#cccccc"],
+//     ["#cccccc", "#cccccc", "#cccccc", "#cccccc", "#cccccc"],
+//     ["#cccccc", "#cccccc", "#cccccc", "#cccccc", "#cccccc"],
+//     ["#cccccc", "#cccccc", "#cccccc", "#cccccc", "#cccccc"],
+//     ["#cccccc", "#cccccc", "#cccccc", "#cccccc", "#cccccc"],
+//   ]
+  
+//   // initialize color - light
+//   const bgPrimaryLight = colorSchemeLight[0][0];
+//   const bgSupportLight = colorSchemeLight[0][1];
+//   const textPrimaryLight = colorSchemeLight[0][2];
+//   const textSecondaryLight = colorSchemeLight[0][3];
+//   const supportLight = colorSchemeLight[0][4];
+  
+//   // initialize color - dark
+//   const bgPrimaryDark = colorSchemeLight[0][0];
+//   const bgSupportDark = colorSchemeLight[0][0];
+//   const textPrimaryDark = colorSchemeLight[0][0];
+//   const textSecondaryDark = colorSchemeLight[0][0];
+//   const supportDark = colorSchemeLight[0][0];
+  
+//   // color setter - light
+//   document.documentElement.style.setProperty(
+//     "--color-background-primary-light",
+//     bgPrimaryLight
+//   );
+
+//   document.documentElement.style.setProperty(
+//     "--color-background-support-light",
+//     bgSupportLight
+//   );
+  
+//   // color setter - dark
+//   document.documentElement.style.setProperty(
+//     "--color-background-primary-dark",
+//     bgPrimaryDark
+//   );
+// }
+
+// setInterval(colorChanger, 5000);
+
